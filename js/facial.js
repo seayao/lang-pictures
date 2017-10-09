5799d7019000aa2d3144728bb20835cee5663abf
barnboo <250159440@qq.com>
2017-10-09 16:45:56 +0800

    seayao

---
+++ js/facial.js
@@ -0,0 +1,193 @@
+/**
+ * Created by Administrator on 2017/10/9.
+ */
+//上传图片
+document.getElementById('file').addEventListener('change', function (e) {
+    var file = e.target.files;
+    if (file.length) {
+        file = file[0];
+        var r = new FileReader();
+        r.readAsDataURL(file);
+        r.onload = function (e) {
+            img.src = this.result;
+        };
+        restore();
+    }
+});
+
+var main = document.getElementById('main');
+var img = document.getElementById('img');
+var c = document.getElementById("myCanvas");
+c.width = c.parentNode.offsetWidth;
+var ctx = c.getContext("2d");
+
+//初始化识别任务
+var faceLeft = 0;
+var faceTop = 0;
+var prop = 1;
+var tracker = new tracking.ObjectTracker(['face']);
+tracker.on('track', function (event) {
+    if (!event.data.length) {
+        comBtn.forEach(function (e) {
+            e.classList.add('hide');
+        });
+        $.seaToast("失败", "未检测到人脸！", "error", {
+            stack: true,
+            timeout: 5e3,
+            has_progress: true
+        });
+        $('.compound-tool').css('display','none');
+    }
+    comBtn.forEach(function (e) {
+        e.classList.remove('hide');
+    });
+    //识别成功，关闭动画
+    $("#main-loading").loading({state: "close"});
+    event.data.forEach(function (rect, i) {
+        if (!i) {
+            theFace = rect;
+        }
+        $('.compound-tool').css('display','block');
+        plot(rect.x, rect.y, rect.width, rect.height);
+        ctx.drawImage(img, rect.x * prop, rect.y * prop, rect.width * prop, rect.height * prop, faceLeft, faceTop, rect.width * prop, rect.height * prop);
+        if (faceLeft + (rect.width * prop + 10) < 500) {
+            faceLeft += (rect.width * prop + 10);
+        } else {
+            faceLeft = 0;
+            faceTop += 200;
+        }
+    });
+});
+
+var trackerTask;
+//开始识别
+document.getElementById('track').addEventListener('click', function () {
+    if (!img.src) {
+        $.seaToast("失败", "未检测到照片！", "error", {
+            stack: true,
+            timeout: 5e3,
+            has_progress: true
+        });
+        $('.compound-tool').css('display','none');
+        return null;
+    }
+    //正在识别，开启动画
+    $("#main-loading").loading({state: "open"});
+    restore();
+    c.width = img.width;
+    c.height = img.height;
+    var stepSize = 1.5;
+    var trackMode = 1.5;
+    //console.log("识别步长:", stepSize, "比例因子:" + trackMode);
+    tracker.setStepSize(stepSize);
+    tracker.setScaleFactor(trackMode);
+    if (trackerTask) {
+        trackerTask.stop();
+    }
+    if (img.naturalWidth > img.width) {
+        prop = img.naturalWidth / img.width;
+    } else {
+        prop = 1;
+    }
+    trackerTask = tracking.track('#img', tracker);
+});
+
+//识别区域划线
+function plot(x, y, w, h) {
+    var rect = document.createElement('div');
+    document.querySelector('.view-container').appendChild(rect);
+    rect.classList.add('rect');
+    rect.style.width = w + 'px';
+    rect.style.height = h + 'px';
+    rect.style.left = (img.offsetLeft + x) + 'px';
+    rect.style.top = (img.offsetTop + y) + 'px';
+}
+
+//恢复现场
+function restore() {
+    theFace = null;
+    faceLeft = 0;
+    faceTop = 0;
+    document.getElementById('file').value = '';
+    var rects = document.querySelectorAll('.rect');
+    if (rects) {
+        var i = 0;
+        for (; i < rects.length; i++) {
+            main.removeChild(rects[i]);
+        }
+    }
+}
+
+//清空
+document.getElementById('resetAll').addEventListener('click', function () {
+    $('.compound-tool').css('display','none');
+    img.removeAttribute('src');
+    ctx.clearRect(0, 0, c.width, c.height);
+    comBtn.forEach(function (e) {
+        e.classList.add('hide');
+    });
+    restore();
+});
+
+//图片合成
+var theFace;
+var faceImg = document.getElementById('theFace');
+var faceC = document.getElementById('faceCtx');
+var faceCtx = faceC.getContext('2d');
+var comBtn = document.querySelectorAll('.compound');
+var maleBg = document.getElementById('maleBg');
+var femaleBg = document.getElementById('femaleBg');
+comBtn.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+        var isMale = btn.classList.contains('male');
+        c.width = 400;
+        c.height = 480;
+        ctx.clearRect(0, 0, c.width, c.height);
+        if (theFace) {
+            //开启动画
+            $("#main-loading").loading({state: "open", time: 3e3});
+            faceC.width = theFace.width * prop;
+            faceC.height = theFace.height * prop;
+            faceCtx.clearRect(0, 0, faceC.width, faceC.height);
+            faceCtx.drawImage(img, theFace.x * prop, theFace.y * prop, theFace.width * prop, theFace.height * prop, 0, 0, theFace.width * prop, theFace.height * prop);
+            faceImg.src = faceC.toDataURL("image/jpeg", 1.0);
+            faceImg.loadOnce(function () {
+                if (isMale) {
+                    AlloyImage(this).act("灰度处理").add(
+                        AlloyImage(this.width, this.height, "#808080")
+                            .act("高斯模糊", 4)
+                            .act("色相/饱和度调节", 22, 45, 0, true),
+                        "叠加"
+                    ).replace(this);
+                } else {
+                    AlloyImage(this).act("灰度处理").add(
+                        AlloyImage(this.width, this.height, "#808080")
+                            .act("高斯模糊", 4)
+                            .act("色相/饱和度调节", 28, 55, 0, true),
+                        "叠加"
+                    ).replace(this);
+                }
+                setTimeout(function () {
+                    var newFace = document.getElementById('theFace');
+                    if (isMale) {
+                        ctx.drawImage(newFace, 0, 0, theFace.width * prop, theFace.height * prop, 95, 100, 186, 186);
+                        ctx.drawImage(maleBg, 0, 0);
+                    } else {
+                        ctx.drawImage(newFace, 0, 0, theFace.width * prop, theFace.height * prop, 117, 74, 194, 194);
+                        ctx.drawImage(femaleBg, 0, 0);
+                    }
+                    //关闭动画
+                    $("#main-loading").loading({state: "close"});
+                }, 400);
+            });
+        } else {
+            //关闭动画
+            $("#main-loading").loading({state: "close"});
+            $.seaToast("失败", "未检测到人脸！", "error", {
+                stack: true,
+                timeout: 5e3,
+                has_progress: true
+            });
+        }
+    });
+});
--- js/index.js
+++ js/index.js
@@ -2,22 +2,6 @@
  * Created by Administrator on 2017/9/20.
  */
 $(document).ready(function () {
-    //移动orPC
-    (function browserRedirect() {
-        var sUserAgent = navigator.userAgent.toLowerCase();
-        var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
-        var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
-        var bIsMidp = sUserAgent.match(/midp/i) == "midp";
-        var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
-        var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
-        var bIsAndroid = sUserAgent.match(/android/i) == "android";
-        var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
-        var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
-        if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
-            $("#platformModal").modal('show');
-        }
-    })();
-
     //设置背景色
     $('#color-picker').colorpicker({
         align: 'left',
@@ -39,68 +23,6 @@ $(document).ready(function () {
         focus: true,
         height: 650
     });
-
-    //智能隐藏导航栏
-    var new_scroll_position = 0;
-    var last_scroll_position;
-    //设置滚动多少隐藏
-    var setHeight = 50;
-    var header = document.getElementById("menu-nav");
-    window.addEventListener('scroll', function (e) {
-        last_scroll_position = window.scrollY;
-        // 向下滚动
-        if (new_scroll_position < last_scroll_position && last_scroll_position > setHeight) {
-            header.classList.remove("slideDown");
-            header.classList.add("slideUp");
-            // 向上滚动
-        } else if (new_scroll_position > last_scroll_position) {
-            header.classList.remove("slideUp");
-            header.classList.add("slideDown");
-            $(".web-code-div").css("display", "none");
-        }
-        new_scroll_position = last_scroll_position;
-    });
-
-    //默认隐藏返回顶部按钮
-    $(function () {
-        //移动距离超过一定距离时出现
-        $(window).scroll(function () {
-            if ($(window).scrollTop() > 100) {
-                $(".back-top").fadeIn("normal");
-            } else {
-                $(".back-top").fadeOut("normal");
-            }
-        });
-        //返回顶部
-        $(".back-top").click(function (e) {
-            e.preventDefault();
-            var speed = 500;//滑动的速度
-            $('body,html').animate({scrollTop: 0}, speed);
-            return false;
-        });
-    });
-
-    //点击扫码
-    $(".scan-log").click(function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        $(".web-code-div").fadeToggle("fast");
-    });
-
-    //全屏检测二维码如存在则点击消失
-    $(window).click(function () {
-        $(".web-code-div").fadeOut("fast");
-    });
-
-    //bootstrap禁止点击隐藏下拉框组
-    $(function () {
-        $("ul.dropdown-menu").on("click", "[data-stopPropagation]", function (even) {
-            even.stopPropagation();
-        });
-    });
-
-    //监听文档是否保存，进行shake提示
-    //$("#save").effect("shake");
 
     //初始化滑块
     var $document = $(document);
@@ -134,7 +56,8 @@ $(document).ready(function () {
 function edit() {
     $('#editContent').summernote({
         lang: 'zh-CN',
-        focus: true
+        focus: true,
+        height: 650
     });
 }
 
